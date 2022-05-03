afe3a130cc16e4c4009403638bd2b17ea4acf06f
Beastmasterjean <91993638+Beastmasterjean@users.noreply.github.com>
2022-05-03 14:43:38 -0400

    Changement dans les classes

---
+++ 1533508_soloProject/1533508_soloProject/Model/Cellule.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace _1533508_soloProject.Model
+{
+    internal class Cellule : INotifyPropertyChanged
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        private bool isAlive;
+        private Brush _lifeFormColour;
+        public bool IsAlive 
+        { 
+            get { return isAlive; }
+            set { 
+                isAlive = value; 
+                OnPropertyChanged(); 
+                LifeFormColour = (value) ? Brushes.Black : Brushes.White; 
+            } 
+        }
+
+       
+        public Brush LifeFormColour
+        {
+            get => _lifeFormColour;
+            set
+            {
+                _lifeFormColour = value;
+                OnPropertyChanged();
+
+            }
+        }
+
+
+        public Cellule() { }
+
+
+
+        #region Interface INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
---
+++ 1533508_soloProject/1533508_soloProject/Model/Forme.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1533508_soloProject.Model
+{
+    internal class Forme
+    {
+        List<Forme> formes;
+        public List<Cellule> cellules { get; set; }
+
+        public Forme(List<Cellule> cellules)
+        {
+            this.cellules = cellules;
+        }
+    }
+}
